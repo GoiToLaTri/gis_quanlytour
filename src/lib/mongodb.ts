fbbef4045b94163fdfd35235a7a3bdf0ef91f413
lai-tran-the-vinh <thevinh.development@gmail.com>
2025-10-15 16:50:37 +0700

    Optimize connect to database function

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -1,11 +1,15 @@
 import mongoose from "mongoose";
 
-const connect = () => {
+const connect = async () => {
   if (mongoose.connections[0].readyState) {
     console.log("Already connected to MongoDB");
     return;
   }
-  mongoose.connect(process.env.DATABASE_URL as string);
+  try {
+    await mongoose.connect(process.env.DATABASE_URL as string);
+  } catch (error) {
+    console.log('Failed to connect to database');
+  }
   console.log("Connected to MongoDB");
 };
 

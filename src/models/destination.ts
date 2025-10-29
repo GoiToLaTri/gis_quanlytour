c73b5208bf7816e4152235a128e36b5bf8861226
GoiToLaTri <dthanhtri1111@gmail.com>
2025-10-29 15:01:49 +0700

    fix

--- src/models/destination.ts
+++ src/models/destination.ts
@@ -1,7 +1,6 @@
 import mongoose from "mongoose";
 
 const destinationSchema = new mongoose.Schema({
-  ma_tour: { type: String, required: true },
   ten: { type: String, required: true },
   dia_chi: { type: String, required: true },
   kinh_do: { type: Number, required: true },
